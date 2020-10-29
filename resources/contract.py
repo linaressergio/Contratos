77ba1de54496486838d54871d1b5e34d6e8a309f
Lorena Granados <legranadosb@yahoo.com>
2020-10-29 08:42:38 -0500

    Creación de logica de recursos

---
+++ resources/contract.py
@@ -0,0 +1,37 @@
+from flask import Response, request
+from database.models import Contract
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource
+
+
+class ContractApi(Resource):
+    def get(self):
+        contracts = Contract.objects().to_json()
+        return Response(contract, mimetype="application/json", status=200)
+
+    @jwt_required
+    def post(self):
+        body = request.get_json()
+        contracts = Contract(**body).save()
+        contract_id  = contracts.contract_id 
+        return {'contract_id ': str(contract_id)}, 200
+
+
+class ContractApi(Resource):
+    @jwt_required
+    def put(self, contract_id):
+        body = request.get_json()
+        Contract.objects.get(contract_id=contract_id).update(**body)
+        return '', 200
+
+    @jwt_required
+    def delete(self, contract_id):
+        contract = Contract.objects.get(contract_id=contract_id).delete()
+        return '', 200
+
+    def get(self, id):
+        try:
+            contract = Contract.objects.get(id=id).to_json()
+            return Response(contract, mimetype="application/json", status=200)
+        except Exception as error:
+            return Response(error, status=400, mimetype='application/json')
