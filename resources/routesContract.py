c8d42fbe45ee6adfc695eade5fb10a940c83da70
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-10-29 08:39:19 -0500

    Update routesContract.py

--- resources/routesContract.py
+++ resources/routesContract.py
@@ -1,1 +1,6 @@
-aaa
+from .client import ContractsApi, ContractsApi
+
+
+def initialize_routes(api):
+    api.add_resource(ContractsApi, '/api/clients')
+    api.add_resource(ContractApi, '/api/client/<id>')
