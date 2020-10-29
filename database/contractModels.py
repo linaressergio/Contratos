3357ba4c949337c7c14f8742040137fb21fa28b5
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-10-29 08:32:21 -0500

    Update contractModels.py

--- database/contractModels.py
+++ database/contractModels.py
@@ -1,1 +1,11 @@
-a
+from .db import db
+
+class Contract(db.Document):
+    contract_id = db.StringField(required=True, unique=True)
+    plan = db.StringField(required=True)
+    insurance_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    principal_name = db.StringField(required=True)
+    principal_id = db.StringField(required=True, unique=True)
+    beneficiary = db.ListField(db.StringField(), required=True)
+
