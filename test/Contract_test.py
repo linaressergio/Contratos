bfc4da14c4e1514962ccd81e085f3547fc87238e
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-11-05 18:44:47 -0500

    Update Contract_test.py

--- test/Contract_test.py
+++ test/Contract_test.py
@@ -11,28 +11,6 @@ class ContractTestCase(unittest.TestCase):
         self.assertEqual(self.test_contract.validate_contract(self.test_contract,123),true)
         
 
-    def test_name_add_preexistence(self):
-        self.assertEqual(len(self.test_client.get_all_preexistence()), 0)
-
-        self.test_client.add_preexistence('mock preexistence')
-        self.assertEqual(len(self.test_client.get_all_preexistence()), 1)
-
-        self.assertEqual(self.test_client.get_preexistence(0), 'mock preexistence')
-        self.assertEqual(self.test_client.get_preexistence(1), 'There is no such preexistence')
-
-    def test_name_remove_preexistence(self):
-        self.assertEqual(len(self.test_client.get_all_preexistence()), 0)
-
-        self.test_client.add_preexistence('mock preexistence')
-        self.test_client.add_preexistence('mock preexistence 2')
-        self.test_client.add_preexistence('mock preexistence 3')
-        self.assertEqual(len(self.test_client.get_all_preexistence()), 3)
-        self.test_client.remove_preexistence(1)
-        #self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence')
-        self.assertEqual(self.test_client.get_preexistence(1), 'mock preexistence 3')
-        self.assertEqual(self.test_client.get_preexistence(2), 'There is no such preexistence')
-
-
 if __name__ == '__main__':
     unittest.main()
     #unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
