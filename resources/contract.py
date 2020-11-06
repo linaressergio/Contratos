33f87dc25fe0b9db25b547d197106873579c22ac
Lorena Granados <legranadosb@yahoo.com>
2020-11-05 23:48:00 -0500

    Update contract.py

--- resources/contract.py
+++ resources/contract.py
@@ -14,7 +14,7 @@ class ContractsApi(Resource):
         try:
             body = request.get_json()
             print(body)
-            contracts = Contract.save(body)
+            contracts = Contract.save({"contract_id" : "1"})
             print("2")
             contract_id  = contracts.contract_id 
             print("3")
