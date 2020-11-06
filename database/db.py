8a7dcc414c9aa9c040db1e3761981270df6f22fa
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-11-05 23:55:48 -0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,10 +1,11 @@
-""" importando MongoEngine 
+""" Importando MongoEngine
     """
 from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
 
-""" iniciando aplicacion 
+""" Iniciando bd
     """
 def initialize_db(app):
+    """Iniciando app """
     db.init_app(app)
