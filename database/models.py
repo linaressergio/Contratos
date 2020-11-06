59e06f2515a71c69b36a2d1364d100f4d752bb54
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-11-05 23:50:29 -0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,8 +1,8 @@
-""" importando base de datos 
+""" importando base de datos
     """
 from .db import db
 
-""" creando clase contrato 
+""" creando clase contrato
     """
 class Contract(db.Document):
     contract_id = db.StringField(required=True, unique=True)
