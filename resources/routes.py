4a93a864e251df4279163c200516f35c6eccc07e
Lorena Granados <legranadosb@yahoo.com>
2020-11-05 20:11:58 -0500

    Update routes.py
    Modificación parametro busqueda get

--- resources/routes.py
+++ resources/routes.py
@@ -3,5 +3,5 @@ from .contract import ContractsApi, ContractApi
 
 def initialize_routes(api):
     api.add_resource(ContractsApi, '/api/contracts')
-    api.add_resource(ContractApi, '/api/contract/<id>')
+    api.add_resource(ContractApi, '/api/contract/<contract_id>')
 
