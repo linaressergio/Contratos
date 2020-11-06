9b5e59268ce10abe8d29339ad55aa4a5dfbf0333
Lorena Granados <legranadosb@yahoo.com>
2020-11-05 23:01:06 -0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -6,10 +6,10 @@ from .db import db
     """
 class Contract(db.Document):
     contract_id = db.StringField(required=True, unique=True)
-    plan = db.StringField(required=True)
-    insurance_name = db.StringField(required=True)
-    state = db.StringField(required=True)
-    last_name = db.StringField(required=True)
-    principal_name = db.StringField(required=True)
-    principal_id = db.StringField(required=True, unique=True)
-    beneficiary = db.ListField(db.StringField(), required=True)
+    plan = db.StringField(required=False)
+    insurance_name = db.StringField(required=False)
+    state = db.StringField(required=False)
+    last_name = db.StringField(required=False)
+    principal_name = db.StringField(required=False)
+    principal_id = db.StringField(required=False)
+    beneficiary = db.ListField(db.StringField(), required=False)
